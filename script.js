ba27c079715d99d0f7f66356d9f81f540c89d8fc
ex1master <efloresa@live.com>
2026-07-27 11:25:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ document.getElementById("buscar").addEventListener("click", () => {
 
         <h2>✅ Tarjeta válida</h2>
 
-        <p><b>Nombre:</b> ${socio.nombre}</p>
+        <p><b>Numero de Tarjeta:</b> ${socio.nombre}</p>
 
         <p><b>Estado:</b> ${socio.estado}</p>
 
